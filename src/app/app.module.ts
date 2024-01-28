79cfe282a5524fd13432100e5ba195b652e89248
Tania024 <84852408+Tania024@users.noreply.github.com>
2024-01-28 16:58:39 -0500

    creación de paquetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CitaComponent } from './pages/cita/cita.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MedicoComponent } from './pages/medico/medico.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitaComponent,
+    LoginComponent,
+    MedicoComponent,
+    ServicesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
